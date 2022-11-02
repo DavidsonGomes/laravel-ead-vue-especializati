243306167c1918874eaddde32da6d0416c1d3cb8
Davidson Gomes <davidsongviolao@gmail.com>
2022-11-02 05:28:57 -0300

    Template inicial

---
+++ src/layouts/DefaultTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <HeaderComponent />
+
+        <section>
+            <router-view></router-view>
+        </section>
+
+        <FooterComponent />
+    </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
+export default {
+    name: "DefaultTemplate",
+    components: {
+        HeaderComponent,
+        FooterComponent
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/home/HomeView.vue'
+import MySupportsView from '@/views/supports/MySupportsView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/campus',
+        component: () => import('@/layouts/DefaultTemplate.vue'),
+        children: [
+            {
+                path: 'minhas-duvidas',
+                name: 'Campus.my.supports',
+                component: MySupportsView
+            },
+            {
+                path: '',
+                name: 'Campus.home',
+                component: HomeView
+            }
+        ]
+    }
 ]
 
 const router = createRouter({
