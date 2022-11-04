fce6956ccf30bdd8ca0493549faad1424099958c
Davidson Gomes <davidsongviolao@gmail.com>
2022-11-04 05:24:14 -0300

    layout minhas duvidas

--- src/views/supports/MySupportsView.vue
+++ src/views/supports/MySupportsView.vue
@@ -1,13 +1,45 @@
 <template>
-    <div class="home">
-      Minha dúvidas
+  <page-title-component :pageTitle="'Minhas Dúvidas'" />
+
+  <div class="content">
+    <div class="container">
+      <div class="left">
+        <div class="card">
+          <div class="title bg-laravel">
+            <span class="text">Filtros</span>
+          </div>
+          <div class="modules">
+            <ul class="classes">
+              <li>Todos</li>
+              <li>Aguardando Minha Resposta</li>
+              <li>Aguardando Professor</li>
+              <li>Finalizados</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="right">
+        <div class="content">
+          <div class="comments">
+            <supports-component />
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
+  </div>
+</template>
   
   <script>
-  
-  export default {
-    name: 'MySupportsView'
-  }
-  </script>
+import PageTitleComponent from "@/components/PageTitleComponent.vue";
+import SupportsComponent from "@/components/SupportsComponent.vue";
+
+export default {
+  name: "MySupportsView",
+  components: {
+    PageTitleComponent,
+    SupportsComponent,
+  },
+};
+</script>
   
