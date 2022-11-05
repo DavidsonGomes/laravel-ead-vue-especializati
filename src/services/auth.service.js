ae0c3146dfa4621a2272f8873dbf74f976d513db
Davidson Gomes <davidsongviolao@gmail.com>
2022-11-05 07:26:10 -0300

    Axios Config

---
+++ src/services/auth.service.js
@@ -0,0 +1,13 @@
+import BaseService from "./base.service";
+
+export default class AuthService extends BaseService {
+
+    static async auth(params) {
+        return new Promise((resolve, reject) => {
+            this.request()
+                .post("/auth", params)
+                .then(response => resolve(response))
+                .catch(error => reject(error));
+        })
+    }
+}
